c2ecf47e5c693b60f0ef4ae32196d051be4017a7
VasiliyMac <cartario@yandex.ru>
2020-10-29 11:11:06 +0300

    Updates

---
+++ static/js/main.41891d25.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpcodix=this.webpackJsonpcodix||[]).push([[0],{10:function(t,e,n){"use strict";n.r(e);var c=n(0),s=n(1),o=n.n(s),i=n(3),r=n.n(i);n(9);var a=function(){return Object(c.jsx)("div",{className:"App",children:Object(c.jsx)("button",{className:"App__button button",children:"\u041d\u0430\u043b\u043e\u0433\u043e\u0432\u044b\u0439 \u0432\u044b\u0447\u0435\u0442..."})})};r.a.render(Object(c.jsx)(o.a.StrictMode,{children:Object(c.jsx)(a,{})}),document.getElementById("root"))},9:function(t,e,n){}},[[10,1,2]]]);
+//# sourceMappingURL=main.41891d25.chunk.js.map
