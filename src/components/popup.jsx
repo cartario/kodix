31f80bfd52662a05da2528391c6a827530f49772
VasiliyMac <cartario@yandex.ru>
2020-10-29 18:37:45 +0300

    верстка и адаптив 3 экран

--- src/components/popup.jsx
+++ src/components/popup.jsx
@@ -1,15 +1,19 @@
 import React, {useState, useEffect} from 'react';
 import closeImg from '../img/vector.png';
+import checkboxImg from '../img/checkbox.png';
 
 const Popup = ({onClose}) => {
   const [form, setForm] = useState("");
+  const rub = '₽';
 
   const handleClosePopup = () => {
     onClose(true);
   }
 
   const handleChange = (e) => {
-    setForm(e.target.value)
+    const value = e.target.value;
+    
+    setForm(value);    
   }
 
   useEffect((e)=>{
@@ -25,7 +29,7 @@ const Popup = ({onClose}) => {
   return (
     <div className="App__popup popup">
       <div>
-      
+      <div>      
         <img onClick={handleClosePopup} src={closeImg}/>
           <h2>Налоговый вычет</h2>
           <p className="popup__description">Используйте налоговый вычет чтобы погасить ипотеку досрочно. 
@@ -35,11 +39,58 @@ const Popup = ({onClose}) => {
             <label htmlFor="salary">Ваша зарплата в месяц</label>
             <input
               id="salary"
-              name="salary"
+              name="salary"              
               value={form}
+              type="number"
               onChange={handleChange}
               placeholder="Введите данные"
             />
+          </div>
+          
+          <div className="popup__checkboxes checkboxes">
+            <h3 className="checkboxes__title">Итого можете внести в качестве досрочных:</h3>
+            <ul className="checkboxes__list">
+              <li className="checkboxes__item checkbox">
+                <input                
+                  className="checkbox__field"
+                  id="year-1"
+                  type="checkbox"/><span></span>
+                <label
+                  className="checkbox__label" 
+                  htmlFor="year-1">78 000 рублей <span>в 1-ый год</span>
+                </label>
+              </li>
+              <li className="checkboxes__item checkbox">
+                <input 
+                  className="checkbox__field"
+                  id="year-2"
+                  type="checkbox"/><span></span>
+                <label
+                  className="checkbox__label" 
+                  htmlFor="year-2">78 000 рублей <span>во 2-ой год</span>
+                </label>
+              </li>
+              <li className="checkboxes__item checkbox">
+                <input 
+                  className="checkbox__field"
+                  id="year-3"
+                  type="checkbox"/><span></span>
+                <label
+                  className="checkbox__label" 
+                  htmlFor="year-3">78 000 рублей <span>во 2-ой год</span>
+                </label>
+              </li>
+              <li className="checkboxes__item checkbox">
+                <input 
+                  className="checkbox__field"
+                  id="year-4"
+                  type="checkbox"/><span></span>
+                <label
+                  className="checkbox__label" 
+                  htmlFor="year-4">78 000 рублей <span>во 2-ой год</span>
+                </label>
+              </li>
+            </ul>
           </div>
           <div className="popup__type">
             <p>Что уменьшаем?</p>
@@ -48,10 +99,10 @@ const Popup = ({onClose}) => {
               <button className="popup__button popup__button--outlined">Срок</button>
             </div>
           </div>
-          </div>
-          <button className="button button--add">Добавить</button>
-        
       </div>
+      <button className="button button--add">Добавить</button>  
+      </div>      
+    </div>
   );
 };
 
