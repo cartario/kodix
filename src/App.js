4ba277b58a8b2ea1468b97b1c17d3e71accb4fa7
VasiliyMac <cartario@yandex.ru>
2020-10-29 13:43:53 +0300

    адаптив 1экран - моблка и десктоп

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   };
 
   return (
-    <div className="App">
+    <div className={visible? "App opened" : "App"}>
       <button onClick={handleToggle}className="App__button button">Налоговый вычет...</button>
       {visible && <Popup onClose={handleToggle}/>}
     </div>
--- src/components/popup.jsx
+++ src/components/popup.jsx
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import closeImg from '../img/vector.png';
 
 const Popup = ({onClose}) => {
   const [form, setForm] = useState("");
@@ -23,21 +24,33 @@ const Popup = ({onClose}) => {
 
   return (
     <div className="App__popup popup">
-        <div onClick={handleClosePopup}>close</div>
-        <h2>Налоговый вычет</h2>
-        <p>Используйте налоговый вычет чтобы погасить ипотеку досрочно. 
-          Размер налогового вычета составляет не более 13% от своего официального годового дохода.
-        </p>
-        <div>
-          <label htmlFor="salary">Ваша зарплата в месяц</label>
-          <input
-            id="salary"
-            name="salary"
-            value={form}
-            onChange={handleChange}
-            placeholder="введите данные"
-          />
-        </div>
+      <div>
+      
+        <img onClick={handleClosePopup} src={closeImg}/>
+          <h2>Налоговый вычет</h2>
+          <p className="popup__description">Используйте налоговый вычет чтобы погасить ипотеку досрочно. 
+            Размер налогового вычета составляет не более 13% от своего официального годового дохода.
+          </p>
+          <div className="popup__salary">
+            <label htmlFor="salary">Ваша зарплата в месяц</label>
+            <input
+              id="salary"
+              name="salary"
+              value={form}
+              onChange={handleChange}
+              placeholder="Введите данные"
+            />
+          </div>
+          <div className="popup__type">
+            <p>Что уменьшаем?</p>
+            <div className="popup__buttons">
+              <button className="popup__button popup__button--filled">Платеж</button>
+              <button className="popup__button popup__button--outlined">Срок</button>
+            </div>
+          </div>
+          </div>
+          <button className="button button--add">Добавить</button>
+        
       </div>
   );
 };
